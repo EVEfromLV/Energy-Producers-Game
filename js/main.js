d721fa879dcb1e8e3de4cc50722e18c82d4d46bb
EVEfromLV <eve.silina@yahoo.com>
2016-05-31 11:42:14 +0200

    adding a new bg image for loading

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ var stage;
 var gameBg;
 var alertBg;
 var score;
-var energy = [biofuel.svg, coal.svg, fuel.svg, geothermal.svg, hydro.svg, naturalGas.svg, nuclear.svg, solar.svg, wind.svg];
+var energy = [];
 var lastEnergy;
 
 var energyX = [80, 198, 338, 70, 225, 376, 142, 356];
@@ -19,10 +19,13 @@ var gameIsRunning = false;
 function preLoad(){
     stage = new createjs.Stage("WhackField");
 
-    preLoadText = new createjs.Text("0%", "40px Helvetica", "brown");
+    var preLoadBg = new createjs.Bitmap("img/preload_bg.jpg");
+    stage.addChild(preLoadBg);
+
+    preLoadText = new createjs.Text("0%", "40px Helvetica", "white");
     stage.addChild(preLoadText);
-    preLoadText.x = stage.canvas.width-490;
-    preLoadText.y = stage.canvas.height/3.1;
+    preLoadText.x = stage.canvas.width-430;
+    preLoadText.y = stage.canvas.height/1.8;
 
     queue = new createjs.LoadQueue(true);
     queue.on("progress", progressIs);
@@ -41,12 +44,21 @@ function preLoad(){
         "img/bg.jpg",
         "img/start_bg.jpg",
         "img/start_btn.png",
-        "img/rules_btn.png"
+        "img/rules_btn.png",
+        "img/preload_bg.jpg"
         // audio also goes in
     ])
 }
 
+function progressIs(e) {
+    preLoadText.text = Math.round(e.progress*100) + "%";
+    stage.update();
+}
+
 function showTitle() {
+    createjs.Ticker.setFPS(60);
+    createjs.Ticker.addEventListener("tick", tock);
+
     var titleBg = new createjs.Bitmap("img/start_bg.jpg");
     stage.addChild(titleBg);
     
@@ -56,8 +68,9 @@ function showTitle() {
     var rulesBtn = new createjs.Bitmap("img/rules_btn.png");
     stage.addChild(rulesBtn);
 
-    var titleView = new Container();
+    var titleView = new createjs.Container();
     container.addChild(titleBg, startBtn, rulesBtn);
+    stage.addChild(titleView);
 
     rulesBtn.addEventListener('click',
         function(e){
@@ -65,23 +78,17 @@ function showTitle() {
             // need to show rules of the game/////////////////
         });
 
-
     startBtn.addEventListener('click',
         function(e){
             stage.removeChild(e.target);
             stage.removeChild(titleView);
+
             gameIsRunning = true;
+            startGame();
         });
 }
 
-function progressIs(e) {
-    preLoadText.text = Math.round(e.progress*100) + "%";
-    stage.update();
-}
-
 function startGame() {
-    createjs.Ticker.setFPS(60);
-    createjs.Ticker.addEventListener("tick", tock);
 
     gameBg = createjs.Bitmap("img/bg.jpg");
     stage.addChild(gameBg);
@@ -89,6 +96,7 @@ function startGame() {
 
 function tock (e) {
     if(gameIsRunning===true){
+        startGame();
     }
     stage.update(e);
 }
